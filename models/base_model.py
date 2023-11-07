9a18cdf16f99bd37488fc0a6dc9e206c21f79fe7
Atala2000 <atalakidi@gmail.com>
2023-11-07 21:05:10 +0300

    can now create instance from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,15 +5,24 @@ BAse Model that contains framework for the project
 from uuid import uuid4
 from datetime import datetime
 
+date_format = "%Y-%m-%dT%H:%M:%S.%f"
+
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         It initializes the class
         """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+                if key in ["created_at", "updated_at"] and isinstance(value, str):
+                    setattr(self, key, datetime.strptime(value, date_format))
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns spring rep of object"""
@@ -25,9 +34,9 @@ class BaseModel:
 
     def to_dict(self):
         """Returns dict rep of keys and their values"""
-        dict_copy = self.__dict__
+        dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
-        dict_copy["created_at"] = self.created_at.isoformat()
-        dict_copy["updated_at"] = self.updated_at.isoformat()
-
+        for key in ["created_at", "updated_at"]:
+            if key in dict_copy and isinstance(dict_copy[key], datetime):
+                dict_copy[key] = dict_copy[key].isoformat()
         return dict_copy
